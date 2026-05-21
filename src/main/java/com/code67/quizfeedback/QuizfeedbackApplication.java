d83a5186c878f83e1ee6a9d204c14e63005a67fc
Subeen Ban <sban919@aucklanduni.ac.nz>
2026-05-21 16:44:07 +1200

    Add Spring Boot project structure

---
+++ src/main/java/com/code67/quizfeedback/QuizfeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.code67.quizfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizfeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizfeedbackApplication.class, args);
+	}
+
+}
