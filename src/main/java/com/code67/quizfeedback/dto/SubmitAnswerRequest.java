bbd0da5c6da36ccd4fbe4699f5f6733f591e34be
Subeen Ban <sban919@aucklanduni.ac.nz>
2026-05-21 19:16:57 +1200

    feat: add Question model and submit answer DTOs

---
+++ src/main/java/com/code67/quizfeedback/dto/SubmitAnswerRequest.java
@@ -0,0 +1,14 @@
+package com.code67.quizfeedback.dto;
+
+public class SubmitAnswerRequest {
+
+    private String answer;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
