406f7c75989606b860becfd62bc1776e22254ff8
Daniel Kim <kdaniel0315@gmail.com>
2026-05-23 20:46:25 +1200

    feat: tolerate small typos in quiz answers via fuzzy matching

--- src/main/java/com/code67/quizfeedback/FeedbackService.java
+++ src/main/java/com/code67/quizfeedback/FeedbackService.java
@@ -10,17 +10,40 @@ public class FeedbackService {
         String normalisedSubmission = normalise(submittedAnswer);
         String normalisedCorrect = normalise(question.getCorrectAnswer());
 
-        boolean correct = !normalisedSubmission.isEmpty()
-                && normalisedSubmission.equals(normalisedCorrect);
-
-        if (correct) {
-            return new SubmitAnswerResponse(
-                    true,
-                    "Correct. Nice work on " + question.getConcept() + ".",
-                    question.getCorrectExplanation()
-            );
+        if (normalisedSubmission.isEmpty()) {
+            return incorrect(question);
         }
 
+        if (normalisedSubmission.equals(normalisedCorrect)) {
+            return correctExact(question);
+        }
+
+        if (FuzzyMatcher.isClose(normalisedSubmission, normalisedCorrect)) {
+            return correctFuzzy(question);
+        }
+
+        return incorrect(question);
+    }
+
+    private SubmitAnswerResponse correctExact(Question question) {
+        return new SubmitAnswerResponse(
+                true,
+                "Correct. Nice work on " + question.getConcept() + ".",
+                question.getCorrectExplanation()
+        );
+    }
+
+    private SubmitAnswerResponse correctFuzzy(Question question) {
+        return new SubmitAnswerResponse(
+                true,
+                "Correct, with a small typo. The expected spelling is '"
+                        + question.getCorrectAnswer()
+                        + "'. Nice work on " + question.getConcept() + ".",
+                question.getCorrectExplanation()
+        );
+    }
+
+    private SubmitAnswerResponse incorrect(Question question) {
         return new SubmitAnswerResponse(
                 false,
                 "Not quite. Here is a hint to help you think it through.",
